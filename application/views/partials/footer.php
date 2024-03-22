802b8280a32bd69e48a89666f336d0c93a57b3c8
Gefallen29 <gefallen29@gmail.com>
2024-03-22 13:29:23 +0700

    First

---
+++ application/views/partials/footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+	<div class="container my-auto">
+		<div class="copyright text-center my-auto">
+			<span>Repost by © 2024 by Spare Part</a></span>
+		</div>
+	</div>
+</footer>
